28da0cddd3057f9b960cc19a325d3ad30958b21b
J Scott Rumptz <jscott@rumptz.tech>
2021-02-19 12:11:57 -0600

    modularized server

--- server.js
+++ server.js
@@ -1,148 +1,26 @@
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
-const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 // parse incoming string or array data
 app.use(express.urlencoded({extended: true}));
+
 // parse incoming json data
 app.use(express.json());
+
 // make the public folder files static resources
 app.use(express.static('public'));
 
-function filterByQuery(query, animalsArray) {
-    let personalityTraitsArray = [];
-    // animalsArray saved as filteredResults
-    let filteredResults = animalsArray;
-    if (query.personalityTraits) {
-        // Save personalityTraits as a dedicated array.
-        // If personalityTraits is a string, place it into a new array and save.
-        if (typeof query.personalityTraits === 'string') {
-            personalityTraitsArray = [query.personalityTraits];
-        } else {
-            personalityTraitsArray = query.personalityTraits;
-        }
-        // Loop through each trait in the personalityTraits array
-        personalityTraitsArray.forEach(trait => {
-            // Check the trait against each animal in the filteredResults array.
-            // Remember, it is initially a copy of the animalsArray,
-            // but here we're updating it for each trait in the .forEach() loop.
-            // For each trait being targeted by the filter, the filteredResults
-            // array will then contain only the entries that contain the trait,
-            // so at the end we'll have an array of animals that have every one 
-            // of the traits when the .forEach() loop is finished.
-            filteredResults = filteredResults.filter(
-                animal => animal.personalityTraits.indexOf(trait) !== -1
-            );
-        });
-    }
+// This is our way of telling the server that any time a client navigates to <ourhost>/api, 
+// the app will use the router we set up in apiRoutes. If / is the endpoint, then the router 
+// will serve back our HTML routes.
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-
-    if (query.diet) {
-        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
-    }
-    if (query.species) {
-        filteredResults = filteredResults.filter(animal => animal.species === query.species);
-    }
-    if (query.name) {
-        filteredResults = filteredResults.filter(animal => animal.name === query.name);
-    }
-    return filteredResults;
-}
-
-function findById(id, animalsArray) {
-    const result = animalsArray.filter(animal => animal.id === id)[0];
-    return result;
-}
-
-function createNewAnimal(body, animalsArray) {
-    const animal = body;
-    animalsArray.push(animal);
-    // Here, we're using the fs.writeFileSync() method, which 
-    // is the synchronous version of fs.writeFile() and doesn't 
-    // require a callback function. If we were writing to a much 
-    // larger data set, the asynchronous version would be better.
-    fs.writeFileSync(
-        // We want to write to our animals.json file in the data subdirectory, 
-        // so we use the method path.join() to join the value of __dirname, which 
-        // represents the directory of the file we execute the code in, with the 
-        // path to the animals.json file.
-        path.join(__dirname, './data/animals.json'),
-        // Next, we need to save the JavaScript array data as JSON, so we use JSON.stringify() 
-        // to convert it. The other two arguments used in the method, null and 2, are means of 
-        // keeping our data formatted. The null argument means we don't want to edit any of our 
-        // existing data; if we did, we could pass something in there. The 2 indicates we want 
-        // to create white space between our values to make it more readable.
-        JSON.stringify({ animals: animalsArray }, null , 2)
-    );
-
-    //return finished code to post route for response
-    return animal;
-}
-
-function validateAnimal(animal) {
-    if (!animal.name || typeof animal.name !== 'string') {
-      return false;
-    }
-    if (!animal.species || typeof animal.species !== 'string') {
-      return false;
-    }
-    if (!animal.diet || typeof animal.diet !== 'string') {
-      return false;
-    }
-    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
-      return false;
-    }
-    return true;
-}
-
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-});
-
-app.get('/api/animals/:id', (req, res) => {
-    const result = findById(req.params.id, animals);
-    if (result) {
-        res.json(result);
-      } else {
-        res.send(404);
-      }
-});
-
-app.post('/api/animals', (req,res) => {
-    // req.body is where our incoming content will be
-    // set id based on what the next index of the array will be
-    req.body.id = animals.length.toString();
-
-    // if any data in req.body is incorrect, send 400 error back
-    if (!validateAnimal(req.body)) {
-        res.status(400).send('The animal is not properly formatted.');
-    } else {
-        // add animal to json file and animals array in this function
-        const animal = createNewAnimal(req.body, animals);
-        res.json(animal);
-    }
-});
-
-app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/animals', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/animals.html'));
-  });
-
-app.get('/zookeepers', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-  });
-
+// start the server
 app.listen(PORT, () => {
-    console.log(`API sever now on port ${PORT}!`);
+    console.log(`API server now on port ${PORT}!`);
 });
