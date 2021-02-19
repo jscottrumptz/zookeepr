18d65318c1a1f6e6da2a8a1cd442de967aa99965
J Scott Rumptz <jscott@rumptz.tech>
2021-02-19 13:07:42 -0600

    complete

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,4 +1,5 @@
 const $animalForm = document.querySelector('#animal-form');
+const $zookeeperForm = document.querySelector('#zookeeper-form');
 
 const handleAnimalFormSubmit = event => {
   event.preventDefault();
@@ -54,4 +55,49 @@ const handleAnimalFormSubmit = event => {
     });
 };
 
+const handleZookeeperFormSubmit = event => {
+  event.preventDefault();
+
+  // get zookeeper data and organize it
+  const name = $zookeeperForm.querySelector('[name="zookeeper-name"]').value;
+  const age = parseInt($zookeeperForm.querySelector('[name="age"]').value);
+  const favoriteAnimal = $zookeeperForm.querySelector('[name="favorite-animal"]').value;
+
+  // create object from form data
+  const zookeeperObj = { name, age, favoriteAnimal };
+
+  console.log(zookeeperObj);
+
+  // Because the request is coming from the server, we don't have to specify the full URL.
+  fetch('api/zookeepers', {
+    // First and most importantly, we have to actually specify what type of request 
+    // it is and set the method to POST. This will allow the request to make it to the 
+    // proper endpoint in our server.
+    method: 'POST',
+    //Next, we have to tell the request what type of data we're looking to send and then 
+    // actually provide the data. We set the headers property to inform the request that 
+    // this is going to be JSON data. That way, we can add stringified JSON data for our 
+    // animalObject to the body property of the request.
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(zookeeperObj)
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      alert('Error: ' + response.statusText);
+    })
+    .then(postResponse => {
+      console.log(postResponse);
+      alert('Thank you for adding a zookeeper!');
+    });
+
+  
+
+};
+
+$zookeeperForm.addEventListener('submit', handleZookeeperFormSubmit);
 $animalForm.addEventListener('submit', handleAnimalFormSubmit);
