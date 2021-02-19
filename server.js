0e4d6c4d14f175e23c6de2abc3afcdf77bf8d937
J Scott Rumptz <jscott@rumptz.tech>
2021-02-19 10:08:22 -0600

    POST

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+// parse incoming json data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -49,6 +56,47 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    // Here, we're using the fs.writeFileSync() method, which 
+    // is the synchronous version of fs.writeFile() and doesn't 
+    // require a callback function. If we were writing to a much 
+    // larger data set, the asynchronous version would be better.
+    fs.writeFileSync(
+        // We want to write to our animals.json file in the data subdirectory, 
+        // so we use the method path.join() to join the value of __dirname, which 
+        // represents the directory of the file we execute the code in, with the 
+        // path to the animals.json file.
+        path.join(__dirname, './data/animals.json'),
+        // Next, we need to save the JavaScript array data as JSON, so we use JSON.stringify() 
+        // to convert it. The other two arguments used in the method, null and 2, are means of 
+        // keeping our data formatted. The null argument means we don't want to edit any of our 
+        // existing data; if we did, we could pass something in there. The 2 indicates we want 
+        // to create white space between our values to make it more readable.
+        JSON.stringify({ animals: animalsArray }, null , 2)
+    );
+
+    //return finished code to post route for response
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -64,6 +112,21 @@ app.get('/api/animals/:id', (req, res) => {
       } else {
         res.send(404);
       }
+});
+
+app.post('/api/animals', (req,res) => {
+    // req.body is where our incoming content will be
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        // add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
 });
 
 app.listen(PORT, () => {
